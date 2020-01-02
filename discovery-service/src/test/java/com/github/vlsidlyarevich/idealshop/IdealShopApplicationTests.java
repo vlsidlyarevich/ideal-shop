e50874f322ec2ff2a6e994db3e6226472dab6cbf
Uladzislau Sidliarevich <vlsidlyarevich@gmail.com>
2020-01-02 17:04:33 +0300

    #7 - setup of discovery-service: eureka setup.

---
+++ discovery-service/src/test/java/com/github/vlsidlyarevich/idealshop/IdealShopApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.github.vlsidlyarevich.idealshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdealShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+*/
