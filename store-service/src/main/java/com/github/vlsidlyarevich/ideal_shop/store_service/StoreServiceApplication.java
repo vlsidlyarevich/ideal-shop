6c17ee75e349a33df589342ec2b964c9d55b5567
Uladzislau Sidliarevich <vlsidlyarevich@gmail.com>
2020-01-05 13:32:42 +0300

    #6 - store service setup

---
+++ store-service/src/main/java/com/github/vlsidlyarevich/ideal_shop/store_service/StoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.vlsidlyarevich.ideal_shop.store_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreServiceApplication.class, args);
+	}
+
+}
